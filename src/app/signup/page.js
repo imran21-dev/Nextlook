93462e522d05483db770d179311bcd85fa449c98
Md Imran Parves <89092460+imran21-dev@users.noreply.github.com>
2025-02-23 16:37:58 +0600

    React hook form implement complete on sign up form

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { WordRotate } from "@/components/magicui/word-rotate";
 import friends from "../../images/friends.png";
 import content from "../../images/content.png";
@@ -12,33 +12,43 @@ import { LuLock, LuCircleUser } from "react-icons/lu";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import { useState } from "react";
 import Link from "next/link";
-import { GridPattern } from "@/components/magicui/grid-pattern";
-import { cn } from "@/lib/utils";
 import { ClockLoader } from "react-spinners";
-
+import { useForm } from "react-hook-form";
+import { IoIosCheckmark } from "react-icons/io";
 
 const Page = () => {
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
   const handlePassword = () => {
-    setShowPassword(!showPassword)
-  }
+    setShowPassword(!showPassword);
+  };
 
-  const [spin, setSpin] = useState(false)
-  const handleSignUp = (e) => {
-    e.preventDefault()
-    setSpin(true)
-    console.log(0)
-  }
-  
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const [spin, setSpin] = useState(false);
+  const password = watch("password", "");
+  const requirements = [
+    { label: "Minimum 8 characters", regex: /.{8,}/ },
+    { label: "At least one uppercase letter", regex: /[A-Z]/ },
+    { label: "At least one lowercase letter", regex: /[a-z]/ },
+    { label: "At least one number", regex: /\d/ },
+    { label: "At least one special character (@$!%*?&)", regex: /[@$!%*?&]/ },
+  ];
+  const onSubmit = (data) => {
+    console.log("Valid Input:", data);
+  };
 
   return (
     <div className="h-screen overflow-hidden grid grid-cols-5 justify-items-center p-20 gap-6 relative">
       <div className="absolute opacity-20 inset-0 -z-10 rounded-full bg-primary blur-[300px] w-[800px] h-[800px] left-1/2 -top-2 -translate-x-1/2 -translate-y-1/2" />
-    
 
       {/* Text section */}
       <section className="w-full col-span-2">
-        <h1 className="logo text-4xl">Nextlook</h1>
+        <h1 className="logo text-4xl text-primary">Nextlook</h1>
         <WordRotate
           className="text-3xl font-bold py-1"
           words={[
@@ -115,9 +125,9 @@ const Page = () => {
           </div>
 
           {/* Input fields */}
-          <form onSubmit={handleSignUp}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <section className="flex justify-between gap-3">
-              <div className="flex flex-col pb-7  w-2/4">
+              <div className="flex flex-col pb-7 relative  w-2/4">
                 <label className="text-xs md:text-sm pb-1">First Name</label>
                 <div className="flex items-center bg-transparentbg border border-transparentbg rounded-lg overflow-hidden px-2 gap-2">
                   <RiUserReceivedLine className="opacity-80" />
@@ -125,11 +135,31 @@ const Page = () => {
                     type="text"
                     placeholder="Mohammad"
                     className="bg-transparent w-full placeholder:opacity-40 focus:outline-none   py-1 "
+                    {...register("firstName", {
+                      required: "First name is required",
+                      pattern: {
+                        value: /^[A-Za-z]+$/,
+                        message: "First name must contain only letters",
+                      },
+                      minLength: {
+                        value: 2,
+                        message: "Must be at least 2 characters",
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: "Must be at most 20 characters",
+                      },
+                    })}
                   />
                 </div>
+                {errors.firstName && (
+                  <p className="text-red-500 pt-1 absolute bottom-2 text-[10px] md:text-xs">
+                    {errors.firstName.message}
+                  </p>
+                )}
               </div>
 
-              <div className="flex flex-col pb-7  w-2/4">
+              <div className="flex flex-col pb-7 relative  w-2/4">
                 <label className="text-xs md:text-sm pb-1">Last Name</label>
                 <div className="flex items-center bg-transparentbg border border-transparentbg rounded-lg overflow-hidden px-2 gap-2">
                   <RiUserSharedLine className="opacity-80" />
@@ -137,12 +167,32 @@ const Page = () => {
                     type="text"
                     placeholder="Imran"
                     className="bg-transparent w-full placeholder:opacity-40 focus:outline-none   py-1 "
+                    {...register("lastName", {
+                      required: "Last name is required",
+                      minLength: {
+                        value: 2,
+                        message: "Minimum 2 characters required",
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: "Maximum 20 characters allowed",
+                      },
+                      pattern: {
+                        value: /^[A-Za-z]+$/,
+                        message: "Only letters are allowed",
+                      },
+                    })}
                   />
                 </div>
+                {errors.lastName && (
+                  <p className="text-red-500 pt-1 absolute bottom-2 text-[10px] md:text-xs">
+                    {errors.lastName.message}
+                  </p>
+                )}
               </div>
             </section>
 
-            <div className="flex flex-col pb-7">
+            <div className="flex flex-col pb-7 relative">
               <label className="text-xs md:text-sm pb-1">Username</label>
               <div className="flex items-center bg-transparentbg border border-transparentbg rounded-lg overflow-hidden px-2 gap-2">
                 <LuCircleUser className="opacity-80" />
@@ -150,11 +200,31 @@ const Page = () => {
                   type="text"
                   placeholder="Username"
                   className="bg-transparent w-full placeholder:opacity-40 focus:outline-none   py-1 "
+                  {...register("username", {
+                    required: "Username is required",
+                    minLength: {
+                      value: 3,
+                      message: "Minimum 3 characters required",
+                    },
+                    maxLength: {
+                      value: 15,
+                      message: "Maximum 15 characters allowed",
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z0-9_]+$/,
+                      message: "Only letters, numbers, and underscores allowed",
+                    },
+                  })}
                 />
               </div>
+              {errors.username && (
+                <p className="text-red-500 pt-1 absolute bottom-2 text-[10px] md:text-xs">
+                  {errors.username.message}
+                </p>
+              )}
             </div>
 
-            <div className="flex flex-col pb-7">
+            <div className="flex flex-col pb-7 relative">
               <label className="text-xs md:text-sm pb-1">Email</label>
               <div className="flex items-center bg-transparentbg border border-transparentbg rounded-lg overflow-hidden px-2 gap-2">
                 <HiOutlineMail className="opacity-80" />
@@ -162,11 +232,24 @@ const Page = () => {
                   type="email"
                   placeholder="Email"
                   className="bg-transparent w-full placeholder:opacity-40 focus:outline-none   py-1 "
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Invalid email address",
+                    },
+                  })}
                 />
               </div>
+              {errors.email && (
+                <p className="text-red-500 pt-1 absolute bottom-2 text-[10px] md:text-xs">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
-            <div className="flex flex-col pb-7">
+            <div className="flex flex-col pb-7 relative">
               <label className="text-xs md:text-sm pb-1">Password</label>
               <div className="flex items-center bg-transparentbg border border-transparentbg rounded-lg overflow-hidden px-2 gap-2">
                 <LuLock className="opacity-80" />
@@ -174,19 +257,74 @@ const Page = () => {
                   type={!showPassword ? "password" : "text"}
                   placeholder="Password"
                   className="bg-transparent w-full placeholder:opacity-40 focus:outline-none   py-1 "
+                  {...register("password", {
+                    required: "Password is required",
+                    pattern: {
+                      value:
+                        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/,
+                      message: "Password must meet all requirements",
+                    },
+                  })}
                 />
-                <span onClick={handlePassword} className="opacity-80 hover:bg-transparentbg cursor-pointer duration-150 w-8 h-8 p-2 rounded-full">{showPassword ? <VscEye/> : <VscEyeClosed/>}</span>
+                <span
+                  onClick={handlePassword}
+                  className="opacity-80 hover:bg-transparentbg cursor-pointer duration-150 w-8 h-8 p-2 rounded-full"
+                >
+                  {showPassword ? <VscEye /> : <VscEyeClosed />}
+                </span>
               </div>
-              <h6 className="text-xs pt-1 opacity-70 font-light">Minimum length is 8 characters</h6>
+              {errors.password && (
+                <p className="text-red-500 pt-1 absolute bottom-2 text-[10px] md:text-xs">
+                  {errors.password.message}
+                </p>
+              )}
+              <div className="pt-2">
+                {requirements.map((req, index) => (
+                  <p
+                    key={index}
+                    className={`text-sm flex items-center ${
+                      req.regex.test(password)
+                        ? "text-primary md:text-xs text-[10px] font-light"
+                        : "opacity-60 md:text-xs text-[10px] font-light"
+                    }`}
+                  >
+                    {req.regex.test(password)}
+                    <IoIosCheckmark /> {req.label}
+                  </p>
+                ))}
+              </div>
             </div>
-            <button disabled={spin && true} className="flex items-center cursor-pointer justify-center gap-2 w-full rounded-lg py-2 hover:bg-primary bg-primary-deep text-background font-semibold md:text-sm  text-xs">{spin && <ClockLoader size={18}/>} Sign Up</button>
+            <button
+              type="submit"
+              disabled={spin && true}
+              className="flex items-center cursor-pointer justify-center gap-2 w-full rounded-lg py-2 hover:bg-primary bg-primary-deep text-background font-semibold md:text-sm  text-xs"
+            >
+              {spin && <ClockLoader size={18} />} Sign Up
+            </button>
           </form>
-          <h2 className="text-center md:text-sm text-xs opacity-70 font-light pt-5">By creating an account, you agree to the <Link href='/login' className="text-primary-deep font-semibold hover:underline">Terms of Service</Link> We&apos;ll accasionaly sen you account related emails.</h2>
-          <h2 className="text-center md:text-sm text-xs opacity-80 font-light pt-5">Already have an account? <Link href='/login' className="text-primary-deep font-normal hover:underline">Login</Link></h2>
+          <h2 className="text-center md:text-sm text-xs opacity-70 font-light pt-5">
+            By creating an account, you agree to the{" "}
+            <Link
+              href="/login"
+              className="text-primary-deep font-semibold hover:underline"
+            >
+              Terms of Service
+            </Link>{" "}
+            We&apos;ll accasionaly sen you account related emails.
+          </h2>
+          <h2 className="text-center md:text-sm text-xs opacity-80 font-light pt-5">
+            Already have an account?{" "}
+            <Link
+              href="/login"
+              className="text-primary-deep font-normal hover:underline"
+            >
+              Login
+            </Link>
+          </h2>
           <BorderBeam
             duration={10}
             size={200}
-            className="from-transparent via-green-500 to-transparent"
+            className="from-transparent via-primary to-transparent"
           />
         </div>
       </section>
